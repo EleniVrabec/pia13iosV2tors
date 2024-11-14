fda3e2106e2b867dfd7bc2f2c73444862b69cba8
EleniVrabec <eleni.vrabec@gritacademy.se>
2024-11-14 09:13:39 +0100

    Initial Commit

---
+++ pia13iosV2tors/pia13iosV2torsApp.swift
@@ -0,0 +1,17 @@
+//
+//  pia13iosV2torsApp.swift
+//  pia13iosV2tors
+//
+//  Created by Eleni Vrabec on 2024-11-14.
+//
+
+import SwiftUI
+
+@main
+struct pia13iosV2torsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
